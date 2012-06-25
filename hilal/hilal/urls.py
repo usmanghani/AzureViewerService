b56d48f1c624f9fbba618e88718f650ae7c467ba
Usman Ghani <usman.ghani@gmail.com>
2012-06-24 19:44:10 -0700

    Yayy. Finally the static docs page work.

--- hilal/hilal/urls.py
+++ hilal/hilal/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'hilal.views.home', name='home'),
+    url(r'^$', 'hilal.views.home', name='home'),
     # url(r'^hilal/', include('hilal.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- hilal/hilal/views.py
+++ hilal/hilal/views.py
@@ -4,34 +4,24 @@ from django.core import serializers
 from praytimes import PrayTimes
 from datetime import date, timedelta
 
+import os
+import os.path
 import requests
 import simplejson
 
-p = PrayTimes('Karachi')
-p.setMethod('Karachi')
-p.adjust({'asr':'Hanafi'})
 
-def pray(request, location):
-	if location:
-		r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location)
-		geocode = simplejson.loads(r.text)
+DEFAULT_METHOD = 'Karachi'
+DEFAULT_ASR = 'Hanafi'
 
-		address = geocode['results'][0]['formatted_address']
-		loc = geocode['results'][0]['geometry']['location']
-		lat = loc['lat']
-		lng = loc['lng']
-		
-		requested_date = date.today()
-		times = p.getTimes(requested_date, (lat, lng), -8, True)
-		response = {'date' : requested_date.strftime("%Y-%m-%d"), 'address': address, 'latitude' : lat, 'longitude' : lng, 'times': times}
+p = PrayTimes(DEFAULT_METHOD)
+p.setMethod(DEFAULT_METHOD)
+p.adjust({'asr':DEFAULT_ASR})
 
-		return HttpResponse(simplejson.dumps(response), content_type='application/json')
-	else:
-		return HttpResponse(status=400)
-
-def pray_date(request, year, month, day, location):
+def calculate_pray(start_date, end_date, location, method, asr_method):
 	try:
 		if location:
+			p.setMethod(method)
+			p.adjust({'asr':asr_method})
 			r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location)
 			geocode = simplejson.loads(r.text)
 
@@ -39,70 +29,54 @@ def pray_date(request, year, month, day, location):
 			loc = geocode['results'][0]['geometry']['location']
 			lat = loc['lat']
 			lng = loc['lng']
-
-			requested_date = date(int(year), int(month), int(day))
 			
-			times = p.getTimes(requested_date, (lat, lng), -8, True)
+			day_count = (end_date - start_date).days + 1
 			
-			response = {'date':requested_date.strftime("%Y-%m-%d"), 'month':month, 'day':day, 'address': address, 'latitude' : lat, 'longitude' : lng, 'times' : times}
+			response = []
+			for single_date in [d for d in (start_date + timedelta(n) for n in range(day_count)) if d < end_date]:
+				times = p.getTimes(single_date, (lat, lng), -8, True)
+				response.append({'date':single_date.strftime("%Y-%m-%d"), 'address': address, 'latitude' : lat, 'longitude' : lng, 'times' : times})
 
 			return HttpResponse(simplejson.dumps(response), content_type='application/json')
 		else:
 			return HttpResponse(status=400)
+	except:
+		return HttpResponse(status=400)
+
+def pray(request, location):
+	try:
+		method = request.GET.get('method', DEFAULT_METHOD)
+		asr_method = request.GET.get('asr', DEFAULT_ASR)
+		return calculate_pray(date.today(), date.today() + timedelta(1), location, method, asr_method)
+	except:
+		return HttpResponse(status=400)
+
+def pray_date(request, year, month, day, location):
+	try:
+		requested_date = date(int(year), int(month), int(day))
+		method = request.GET.get('method', DEFAULT_METHOD)
+		asr_method = request.GET.get('asr', DEFAULT_ASR)
+		return calculate_pray(requested_date, requested_date + timedelta(1), location, method, asr_method)
 	except:
 		return HttpResponse(status=400)
 
 def pray_month(request, year, month, location):
 	try:
-		if location:
-			r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location)
-			geocode = simplejson.loads(r.text)
-
-			address = geocode['results'][0]['formatted_address']
-			loc = geocode['results'][0]['geometry']['location']
-			lat = loc['lat']
-			lng = loc['lng']
-
-			start_date = date(year=int(year), month=int(month), day=1)
-			end_date = date(year=int(year), month=(int(month) + 1), day=1)
-			
-			day_count = (end_date - start_date).days + 1
-			
-			response = []
-			for single_date in [d for d in (start_date + timedelta(n) for n in range(day_count)) if d < end_date]:
-				times = p.getTimes(single_date, (lat, lng), -8, True)
-				response.append({'date':single_date.strftime("%Y-%m-%d"), 'address': address, 'latitude' : lat, 'longitude' : lng, 'times' : times})
-
-			return HttpResponse(simplejson.dumps(response), content_type='application/json')
-		else:
-			return HttpResponse(status=400)
+		start_date = date(year=int(year), month=int(month), day=1)
+		end_date = date(year=int(year), month=(int(month) + 1), day=1)
+		method = request.GET.get('method', DEFAULT_METHOD)
+		asr_method = request.GET.get('asr', DEFAULT_ASR)
+		return calculate_pray(start_date, end_date, location, method, asr_method)
 	except:
 		return HttpResponse(status=400)
 
 def pray_year(request, year, location):
 	try:
-		if location:
-			r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location)
-			geocode = simplejson.loads(r.text)
-
-			address = geocode['results'][0]['formatted_address']
-			loc = geocode['results'][0]['geometry']['location']
-			lat = loc['lat']
-			lng = loc['lng']
-
-			start_date = date(year=int(year), month=1, day=1)
-			end_date = date(year=int(year) + 1, month=1, day=1)
-
-			day_count = (end_date - start_date).days + 1
-			
-			response = []
-			for single_date in [d for d in (start_date + timedelta(n) for n in range(day_count)) if d < end_date]:
-				times = p.getTimes(single_date, (lat, lng), -8, True)
-				response.append({'date':single_date.strftime("%Y-%m-%d"), 'address': address, 'latitude' : lat, 'longitude' : lng, 'times' : times})
-
-			return HttpResponse(simplejson.dumps(response), content_type='application/json')
-		else:
-			return HttpResponse(status=400)
+		start_date = date(year=int(year), month=1, day=1)
+		end_date = date(year=int(year) + 1, month=1, day=1)
+		method = request.GET.get('method', DEFAULT_METHOD)
+		asr_method = request.GET.get('asr', DEFAULT_ASR)
+		return calculate_pray(start_date, end_date, location, method, asr_method)
 	except:
 		return HttpResponse(status=400)
 
@@ -143,3 +117,8 @@ def hilal(request, month):
 
 def hilal_index(request):
 	return hilal(request, month='')
+
+def home(request):
+	with open(os.path.join(os.path.dirname(__file__), 'static/docs.html')) as docs:
+		return HttpResponse(docs.read())
+
